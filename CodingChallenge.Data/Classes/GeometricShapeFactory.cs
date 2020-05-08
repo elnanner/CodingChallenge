26ed4b8b18420901b956f50ae92f29abed326351
Luciano Matsuo <matsuoluciano@gmail.com>
2020-05-08 20:43:19 -0300

    Factorias y modificacion de FormaGeometrica

---
+++ CodingChallenge.Data/Classes/GeometricShapeFactory.cs
@@ -0,0 +1,37 @@
+﻿using CodingChallenge.Data.Classes;
+using CodingChallenge.Data.Entities;
+using System;
+
+namespace CodingChallenge.Data.Contracts
+{
+    public class GeometricShapeFactory
+    {
+        public static IGeometricShape GetGeometricShape(EGeometricShapes eGeometricShapes) {
+            IGeometricShape _iGeometricShape;
+
+            switch (eGeometricShapes)
+            {
+                case EGeometricShapes.Circle:
+                    _iGeometricShape = new Circle();
+                    break;
+
+                case EGeometricShapes.EquilateralTriangle:
+                    _iGeometricShape = new EquilateralTriangle();
+                    break;
+
+                case EGeometricShapes.Square:
+                    _iGeometricShape = new Rectangle();
+                    break;
+
+                case EGeometricShapes.Trapeze:
+                    _iGeometricShape = new Trapeze();
+                    break;
+
+                default:
+                    throw new Exception("Unsupported Geometric Form.");
+            }
+
+            return _iGeometricShape;
+        }
+    }
+}
---
+++ CodingChallenge.Data/Classes/LanguageFactory.cs
@@ -0,0 +1,29 @@
+﻿using CodingChallenge.Data.Classes;
+using CodingChallenge.Data.Entities;
+using System;
+
+namespace CodingChallenge.Data.Contracts
+{
+    public class LanguageFactory
+    {
+        public static ILanguage GetLanguage(ELanguages eLanguages) {
+            ILanguage _iLanguage;
+
+            switch (eLanguages)
+            {
+                case ELanguages.English:
+                    _iLanguage = new English();
+                    break;
+
+                case ELanguages.Spanish:
+                    _iLanguage = new Spanish();
+                    break;
+                    
+                default:
+                    throw new Exception("Unsupported Language.");
+            }
+
+            return _iLanguage;
+        }
+    }
+}
